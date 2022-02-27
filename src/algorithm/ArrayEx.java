3af899ea1137982e1d70db253ac519738bdd9fa2
raon0214 <Abty4022@naver.com>
2022-02-27 15:18:59 +0900

    update:MinMax

---
+++ src/algorithm/ArrayEx.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] score = {79,88,91,33,100,55,99};
+		int max = score[0];
+		int min = score[0];
+		}
+
+}
