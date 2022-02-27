fb4c3762e281b77a5d789e6e4a220854237bb847
raon0214 <Abty4022@naver.com>
2022-02-27 15:26:00 +0900

    update min and max

--- src/algorithm/ArrayEx.java
+++ src/algorithm/ArrayEx.java
@@ -7,6 +7,18 @@ public class ArrayEx {
 		int[] score = {79,88,91,33,100,55,99};
 		int max = score[0];
 		int min = score[0];
+		for(int i = 0 ; i < score.length; i++) {
+			if(max < score[i]) {
+				max = score[i];
+			}
+			if(min > score[i]) {
+				min = score[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println(min);
+
+		
 		}
 
 }
